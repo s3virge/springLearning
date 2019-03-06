84a8827e1c4764b288dab44555aa7c8faefa14e2
s3virge <kobzarvit@gmail.com>
2019-03-06 22:45:12 +0200

    jdk version was changed to 8

---
+++ src/main/java/lesson4/App.java
@@ -0,0 +1,19 @@
+package lesson1;
+
+public class App {
+    private Client client;
+    private ConsoleEventLogger eventLogger;
+
+    private void logEvent(String msg) {
+        String message = msg.replaceAll(client.getId(), client.getName());
+        eventLogger.logEvent(message);
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+
+        app.client = new Client("1", "John Smith");
+        app.eventLogger = new ConsoleEventLogger();
+        app.logEvent("Some event for client 1");
+    }
+}
