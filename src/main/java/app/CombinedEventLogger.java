a447cf3231137d1d51ee625f534f02113e6432d2
s3virge <kobzarvit@gmail.com>
2019-03-12 23:52:23 +0200

    understands with properties files

--- src/main/java/app/CombinedEventLogger.java
+++ src/main/java/app/CombinedEventLogger.java
@@ -6,14 +6,14 @@ public class CombinedEventLogger implements EventLogger {
 
     private Collection<EventLogger> loggers;
 
+    public CombinedEventLogger(Collection<EventLogger> loggers) {
+        this.loggers = loggers;
+    }
+
     @Override
     public void logEvent(Event event) {
         for (EventLogger el: loggers ) {
             el.logEvent(event);
         }
-    }
-
-    public CombinedEventLogger(Collection<EventLogger> loggers) {
-        this.loggers = loggers;
     }
 }
